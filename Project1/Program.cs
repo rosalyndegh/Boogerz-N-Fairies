d1ece60c85038835cf156220641f47095e17ddab
rosalyndegh <rosieghorst@gmail.com>
2014-09-22 21:57:16 -0400

    Initial commit. Adding project files.
    Includes Solution and cs files
    Updated gitignore for Visual Studio

---
+++ Project1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LessonOne();
+            LessonTwo();
+        }
+
+        static void LessonOne()
+        {
+            string words = "Herro World";
+            System.Console.WriteLine(words);
+            while (true)
+            {
+                ConsoleKeyInfo key = System.Console.ReadKey(true);
+
+                if (key.KeyChar == 'g')
+                {
+                    Console.WriteLine("Boogers");
+                }
+                else
+                {
+                    Console.WriteLine("fairy");
+                }
+
+            }
+        }
+
+        static void LessonTwo()
+        {
+            // -> this will be removed when we compile <-
+            int[] alice = new int[] { 3, 4, 5, 6, 7, 8, 9, 10, 15 };
+
+            // "for" loop
+            for (int i = 0; i < alice.Length; i++)
+            {
+                Console.WriteLine(alice[i]);
+                //String message = String.Format("Alice[{0}] : {1}", i, alice[i]);
+                if (alice[i] % 3 == 0)
+                {
+                    Console.Write("fizz");
+                }
+                if (alice[i] % 5 == 0)
+                {
+                    Console.Write("buzz");
+                }
+                if (alice[i] % 3 == 0 || alice[i] % 5 == 0)
+                {
+                    Console.Write("\n"); 
+                }
+                
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
